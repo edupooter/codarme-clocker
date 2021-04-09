96427eb2a267b59ed2f2f56b54371f7053197976
Eduardo Pooter Reis <edupooterreis@gmail.com>
2021-04-08 22:48:46 -0300

    (feat) Adiciona lib para formatar datas; Transforma o Logo em componente.

--- pages/agenda.js
+++ pages/agenda.js
@@ -1,29 +1,54 @@
-import { Button } from '@chakra-ui/react'
+import { Box, Button, Container } from '@chakra-ui/react'
 
 import { useRouter } from 'next/router'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import axios from 'axios'
 
 import { useFetch } from '@refetty/react'
 
-import { useAuth } from '../components'
+import { Logo, useAuth, formatDate } from '../components'
+
+const getAgenda = ({ token, when }) => axios({
+  method: 'get',
+  url: '/api/agenda',
+  params: {
+    when
+  },
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+})
+
+const Header = ({ children }) => (
+  <Box p={4} display='flex' alignItems='center' justifyContent='space-between'>
+    {children}
+  </Box>
+)
 
 export default function Agenda () {
   const [auth, { logout }] = useAuth()
 
   const router = useRouter()
 
+  const [when, setWhen] = useState(() => new Date())
+
+  // const [data, {loading, status, error}, fetch] = useFetch(() => getAgenda(when))
+
   useEffect(() => {
     !auth.user && router.push('/')
   }, [auth.user])
 
   return (
-    <div>
-      <Button onClick={logout}>
-        Sair
-      </Button>
-    </div>
+    <Container>
+      <Header>
+        <Logo size={150} />
+        <Button onClick={logout}>
+          Sair
+        </Button>
+      </Header>
+      <Box>{formatDate(new Date, 'PPPP')}</Box>
+    </Container>
   )
 }
